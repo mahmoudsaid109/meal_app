43d2f07c7f20f604b6bf5566d14614cdf8874536
Mahmoud Said <102619410+mahmoudsaid109@users.noreply.github.com>
2026-08-16 17:14:43 +0300

    finish filters

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,12 +6,17 @@ import 'package:meal_app/screens/meals_screen.dart';
 import 'package:meal_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key, required this.onToggleFavorite});
+  const CategoriesScreen({
+    super.key,
+    required this.onToggleFavorite,
+    required this.availableMeals,
+  });
 
   final void Function(Meal meal) onToggleFavorite;
+  final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = dummyMeals
+    final filteredMeals = availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
